38af0dab9bd0d41636393a0032463b81663d00ab
Gpex-John <edafiokagp@gmail.com>
2022-10-20 20:58:36 -0700

    nested loop early

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,33 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_triangle - entry point
- * Description: Prints diagonals
- * @size: size of the triangle
- * Return: void
+ * main - prints the numbers from 1 to 100
+ * 3 multiples print Fizz instead of the number
+ * 5 multiples print Buzz instead of the number
+ * 3 & 5 multiples print FizzBuzz instead of the number
+ * Return: Always 0 (Success)
  */
-
-void print_triangle(int size)
+int main(void)
 {
-	int row, hashes, spaces;
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
-	if (size <= 0)
+	for (i = 1; i <= 100; i++
 	{
-		_putchar('\n');
-	}
+	if (i == 100)
+		printf("%s", b);
+	else if ((i % 3 == 0) && (i % 5 == 0))
+		printf("%s ", fb);
+	else if (i % 3 == 0)
+		printf("%s ", f);
+	else if (i % 5 == 0)
+		printf("%s ", b);
 	else
-	{
-		for (row = 1; row <= size; row++)
-		{
-			for (spaces = size - row; spaces >= 1; spaces--)
-			{
-				_putchar(' ');
-			}
-			for (hashes = 1; hashes <= row; hashes++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+		printf("%d ", i);
 	}
+	printf("\n");
+	return (0);
 }
