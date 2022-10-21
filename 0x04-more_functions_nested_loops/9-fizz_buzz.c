961a129833d7ae3156e82ef6529a785a09e79234
Gpex-John <edafiokagp@gmail.com>
2022-10-20 21:01:29 -0700

    nested loop early

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,18 +16,18 @@ int main(void)
 	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++
-	{
-	if (i == 100)
-		printf("%s", b);
-	else if ((i % 3 == 0) && (i % 5 == 0))
-		printf("%s ", fb);
-	else if (i % 3 == 0)
-		printf("%s ", f);
-	else if (i % 5 == 0)
-		printf("%s ", b);
-	else
-		printf("%d ", i);
-	}
-	printf("\n");
-	return (0);
+			{
+			if (i == 100)
+			printf("%s", b);
+			else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+			else if (i % 3 == 0)
+			printf("%s ", f);
+			else if (i % 5 == 0)
+			printf("%s ", b);
+			else
+			printf("%d ", i);
+			}
+			printf("\n");
+			return (0);
 }
