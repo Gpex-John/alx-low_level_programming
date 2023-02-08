59831228bf09e4af06b2b0dc760c0b3b81226acd
Gpex-John <edafiokagp@gmail.com>
2023-02-08 13:46:54 -0800

    low level

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 
 /**
- *  * hash_table_get - retrieves a value associated with a key
- *   * @ht: input hash table
- *    * @key: input key
- *     * Return: value associated with the element, or NULL if key
- *      * couldn’t be found
- *       */
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: input hash table
+ * @key: input key
+ * Return: value associated with the element, or NULL if key
+ * couldn’t be found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int idx;
@@ -17,9 +17,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	idx = key_index((const unsigned char *)key, ht->size);
 	curr = ht->array[idx];
 	while (curr)
+	{
 		if (strcmp(curr->key, key) == 0)
 			return (curr->value);
-	curr = curr->next;
-}
+		curr = curr->next;
+	}
 return (NULL);
 }
